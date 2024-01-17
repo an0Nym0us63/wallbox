046b9220fafd4501446e44dddc48167cde3e6760
sarakha63 <sarakha_ludovic@yahoo.fr>
2024-01-17 10:24:57 +0100

    typo

--- app/wallbox/wallbox.go
+++ app/wallbox/wallbox.go
@@ -120,7 +120,7 @@ func (w *Wallbox) RefreshData() {
 		"  `first_car`.`battery` AS car_battery," +
 		"  IF(`active_session`.`unique_id` != 0," +
 		"    'none'," +
-		"    to_char(`latest_session`.`start_time`,'YYYY-MM-DD HH24:MI:SS')) AS end_time," +
+		"    to_char(`latest_session`.`end_time`,'YYYY-MM-DD HH24:MI:SS')) AS end_time," +
 		"  IF(`active_session`.`unique_id` != 0," +
 		"    to_char(`active_session`.`start_timestamp`,'YYYY-MM-DD HH24:MI:SS')," +
 		"    to_char(`latest_session`.`start_time`,'YYYY-MM-DD HH24:MI:SS')) AS start_time," +
